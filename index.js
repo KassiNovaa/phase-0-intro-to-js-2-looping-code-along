460fe36bc36ea97f4872852ac8527d3ee1180c2d
Kassidy Puello <kassidympuello@gmail.com>
2023-09-04 15:10:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(arrayofnames,stringofevent) {
+console.log(`Thank you, ${arrayofnames}`,`${stringofevent}`)
+}
+
+say Thankyou, ("Charlie")
+
+console.log [writeCards(["Charlie", "Samip", "Ali"], "birthday");]
+
+// function sayHello(name){
+// console.log(`hello ${name}`)
+// }
+
+// sayHello("Kass")
+
+
